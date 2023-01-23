a077d5a24369dc1e438e7ad6ed4ca45b247b3882
Terrence Chung <terrencechung35@gmail.com>
2023-01-23 11:01:44 -0800

    added js and html stuff

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+//rick smith,morty smith,summer smith, beth smith, jerry smith
+const url = 'https://rickandmortyapi.com/api/character/'
+const rickSanchez = createEl('rick-sanchez');
+const mortySmith = createEl('morty-smith');
+const bethSmith = createEl('beth-smith');
+const jerrySmith = createEl('jerry-smith');
 
+document.body.appendChild(rickSanchez);
+
+fetch(url)
+.then(resp=>resp.json())
+.then(ricknmorty);
+
+function ricknmorty(data){
+    console.log(data);
+    rickSanchez.src = data.image;
+};
+
+
+
+
+function createEl(id){
+    return document.createElement(id);
+};
