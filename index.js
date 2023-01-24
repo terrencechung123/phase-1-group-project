d5de6cccd8a9ed590e4c2a7ad6786e153627e28a
Terrence Chung <terrencechung35@gmail.com>
2023-01-24 09:18:28 -0800

    working

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 //marcussbranch
 //rick smith,morty smith,summer smith, beth smith, jerry smith
-const url = "https://rickandmortyapi.com/api/character/";
-const rickSanchez = createEl("rick-sanchez");
-const mortySmith = createEl("morty-smith");
-const bethSmith = createEl("beth-smith");
-const jerrySmith = createEl("jerry-smith");
+// const url = "https://rickandmortyapi.com/api/character/";
+// const rickSanchez = createEl("rick-sanchez");
+// const mortySmith = createEl("morty-smith");
+// const bethSmith = createEl("beth-smith");
+// const jerrySmith = createEl("jerry-smith");
 
-document.body.appendChild(rickSanchez);
+// document.body.appendChild(rickSanchez);
 
-fetch(url)
-  .then((resp) => resp.json())
-  .then(ricknmorty);
+// fetch(url)
+//   .then((resp) => resp.json())
+//   .then(ricknmorty);
 
-function ricknmorty(data) {
-  console.log(data);
-  rickSanchez.src = data.image;
-}
+// function ricknmorty(data) {
+//   console.log(data);
+//   rickSanchez.src = data.image;
+// }
 
-function createEl(id) {
-  return document.createElement(id);
-}
+// function createEl(id) {
+//   return document.createElement(id);
+// }
 
-likeBtn.addEventListener("click", (event) => {
-  const newLike = (pageData.likes += 1);
-  likes.textContent = newLike;
-});
-=======
+// likeBtn.addEventListener("click", (event) => {
+//   const newLike = (pageData.likes += 1);
+//   likes.textContent = newLike;
+// });
+// //
 const url = "https://rickandmortyapi.com/api/character/";
 
 const cardsContainer = document.getElementById("card-container");
