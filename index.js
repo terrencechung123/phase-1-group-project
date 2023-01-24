3c6e1fe01408c260bc8b65f61a252248156fd8b3
marcus featherston <marcusovfeatherston@gmail.com>
2023-01-24 09:02:20 -0700

    refactored code

--- index.js
+++ index.js
@@ -1,58 +1,26 @@
-//rick smith,morty smith,summer smith, beth smith, jerry smith
 const url = "https://rickandmortyapi.com/api/character/";
-const rickSanchez = el("rick");
-const mortySmith = el("morty");
-const summerSmith = el("summer");
-const bethSmith = el("beth");
-const jerrySmith = el("jerry");
 
-const rickImage = el("rick-image");
-const mortyImage = el("morty-image");
-const summerImage = el("summer-image");
-const bethImage = el("beth-image");
-const jerryImage = el("jerry-image");
+const cardsContainer = document.getElementById("card-container");
 
 fetch(url)
   .then((resp) => resp.json())
-  .then(ricknmorty);
+  .then((data) => {
+    // console.log(data.results);
+    const characterList = data.results;
+    characterList.forEach((character) => {
+      console.log(character);
+      const card = document.createElement("article");
+      // create and populate name element
+      const nameElement = document.createElement("p");
+      nameElement.textContent = character.name;
+      // create and populate image element
+      const imageElement = document.createElement("img");
+      imageElement.src = character.image;
+      // create place of origin element
+      const originElement = document.createElement("p");
+      originElement.textContent = `Place of origin: ${character.origin.name}`;
 
-function ricknmorty(data) {
-  rickImage.src = data.results[0].image;
-  mortyImage.src = data.results[1].image;
-  summerImage.src = data.results[2].image;
-  bethImage.src = data.results[3].image;
-  jerryImage.src = data.results[4].image;
-
-  // Get a HTML collection of all article elements.
-
-  const articleElements = document.getElementsByTagName("article");
-
-  //Turn the HTML collection into a proper array so we can iterate through it.
-  Array.from(articleElements).forEach((element, index) => {
-    console.log(element, index, data.results);
-    // Use our current index within the array to set the inner text to our data.results index. Because our data comes back in the same order as our HTML.
-    element.innerText = `Place of origin: ${data.results[index].origin.name}`;
+      cardsContainer.append(card);
+      card.append(nameElement, imageElement, originElement);
+    });
   });
-
-  //el("rick-article").innerText = data.results[0].origin.name;
-
-  rick();
-  morty();
-  summer();
-  beth();
-  jerry();
-}
-
-function rick(data) {}
-
-function morty(data) {}
-
-function summer() {}
-
-function beth(data) {}
-
-function jerry(data) {}
-
-function el(id) {
-  return document.getElementById(id);
-}
