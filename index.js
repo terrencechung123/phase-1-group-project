442926e9e7c27fb47911dd10a51b3f0be201a090
marcus featherston <marcusovfeatherston@gmail.com>
2023-01-24 10:43:40 -0700

    added details

--- index.js
+++ index.js
@@ -1,37 +1,8 @@
-//marcussbranch
-//rick smith,morty smith,summer smith, beth smith, jerry smith
-// const url = "https://rickandmortyapi.com/api/character/";
-// const rickSanchez = createEl("rick-sanchez");
-// const mortySmith = createEl("morty-smith");
-// const bethSmith = createEl("beth-smith");
-// const jerrySmith = createEl("jerry-smith");
-
-// document.body.appendChild(rickSanchez);
-
-// fetch(url)
-//   .then((resp) => resp.json())
-//   .then(ricknmorty);
-
-// function ricknmorty(data) {
-//   console.log(data);
-//   rickSanchez.src = data.image;
-// }
-
-// function createEl(id) {
-//   return document.createElement(id);
-// }
-
-// likeBtn.addEventListener("click", (event) => {
-//   const newLike = (pageData.likes += 1);
-//   likes.textContent = newLike;
-// });
-// //
 const url = "https://rickandmortyapi.com/api/character/";
 
 const cardsContainer = document.getElementById("card-container");
 
-cardsContainer.className = 'container'
-
+cardsContainer.className = "container";
 
 fetch(url)
   .then((resp) => resp.json())
@@ -50,10 +21,21 @@ fetch(url)
       // create place of origin element
       const originElement = document.createElement("p");
       originElement.textContent = `Place of origin: ${character.origin.name}`;
-    
-      card.className = 'container'
+      // create species detail
+      const speciesElement = document.createElement("p");
+      speciesElement.textContent = `Species: ${character.species}`;
+      //create status detail
+      const statusElement = document.createElement("p");
+      statusElement.textContent = `Status: ${character.status}`;
+
+      card.className = "container";
       cardsContainer.append(card);
-      card.append(nameElement, imageElement, originElement);
+      card.append(
+        nameElement,
+        imageElement,
+        originElement,
+        speciesElement,
+        statusElement,
+      );
     });
   });
-//main
