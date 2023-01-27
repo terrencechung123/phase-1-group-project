c5c297c8d882564f86dd7185dd598bb17457add3
Terrence Chung <terrencechung35@gmail.com>
2023-01-26 22:34:56 -0800

    tryingToAddAFormThatRendersANewCharacter

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const handleDropDown = (event, container) => {
   event.preventDefault();
   container.className = container.className === "hide" ? "show" : "hide";
 };
+// const form = document.getElementById('createNewCharacterCard');
+// form.addEventListener('submit',(e)=>{
+//   e.preventDefault();
+//   const newCharacterObj = {
+//     name: e.target.name.value,
+//     placeOfOrigin: e.target.name.value,
+//     image: e.target.image.value,
+//     speciesElement: e.target.species.value,
+//     statusElement: e.target.status.value,
+//   };
+//   newCharacterObj.forEach(Ass);
+//     }
+// );
+
 
 fetch(url)
   .then((response) => response.json())
@@ -14,6 +28,7 @@ fetch(url)
     character.forEach(renderCharacterApi);
   });
   
+
 function renderCharacterApi(character){
   //Creating Elements and Modifications Here
   const card = createEl("article");
@@ -30,6 +45,7 @@ function renderCharacterApi(character){
     detailsContainer.className = "hide";
   const originElement = createEl("p");
     originElement.textContent = `Place of origin: ${character.origin.name}`;
+    console.log('originElement', originElement);
   const speciesElement = createEl("p");
     speciesElement.textContent = `Species: ${character.species}`;
   const statusElement = createEl("p");
@@ -48,6 +64,7 @@ function renderCharacterApi(character){
     likeButton.textContent = "♥";
     let likes = 0;
     
+  
     //addEventHandlers() Here
     imageElement.addEventListener(`mouseenter`, () => {
       imageElement.style.border = `12px ridge limegreen`;
@@ -59,22 +76,59 @@ function renderCharacterApi(character){
       likes += 1;
       likeCount.textContent = `${likes} likes`;
     });
+    likeButton.addEventListener('mouseenter',(e)=>{
+      likeButton.style.backgroundColor = 'pink';
+    });
+    likeButton.addEventListener('mouseleave',(e)=>{
+      likeButton.style.backgroundColor = 'greenyellow';
+    });
     dropDownButton.addEventListener('click', (event)=>{
       handleDropDown(event, detailsContainer);
     });
+    dropDownButton.addEventListener('mouseenter',(e)=>{
+      dropDownButton.style.backgroundColor = 'pink';
+    });
+    dropDownButton.addEventListener('mouseleave',(e)=>{
+      dropDownButton.style.backgroundColor = 'greenyellow';
+    });
+  
     
     //Append() Here
     cardsContainer.append(card);
     card.append(nameElement, imageElement, dropDownButton, detailsContainer, likeSection,);
     detailsContainer.append(originElement, speciesElement, statusElement);
     likeSection.append(likeCount, likeButton);
-  };
+
+    // form.addEventListener('submit',(e)=>{
+    //   e.preventDefault();
+    //   const newCharacterObj = {
+    //     name: e.target.name.value,
+    //     placeOfOrigin: e.target.placeOfOrigin.value,
+    //     image: e.target.image.value,
+    //     species: e.target.species.value,
+    //     status: e.target.status.value,
+    //   };
+    //   console.log('newCharacterObj', newCharacterObj);
+    //   renderCharacterApi(newCharacterObj);
+    // });
+  }
+//   function createNewCharacter(CharacterObj){
+//       console.log('new',CharacterObj);
+//       const newCharacterObj = {
+//         name: CharacterObj.name.value,
+//         placeOfOrigin: CharacterObj.placeOfOrigin.value,
+//         image: CharacterObj.image.value,
+//         species: CharacterObj.species.value,
+//         status: CharacterObj.status.value,
+//       };
+//       console.log(image);
+//   }
+// renderCharacterApi(CharacterObj);
   
 //Shortcut Functions Here
 function createEl(id){
   return document.createElement(id);
 }
-  
 //BONUS GOAL:
 //1.Create like button at the bottom right corner of each character card.X
 //2.Have number of likes display next to heart and have it increment by 1 for each click.
